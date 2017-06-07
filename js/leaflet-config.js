a17b2b86fe5455816ad1ff93c7875811c3feb88c
Maxime Colmant <maxime.colmant@gmail.com>
2017-06-07 22:08:54 +0200

    feat: update with last job infos

--- js/leaflet-config.js
+++ js/leaflet-config.js
@@ -1,11 +1,10 @@
-var map = L.map('map').setView([50.60690, 3.15217], 15);
+var map = L.map('map').setView([50.63309, 3.02029], 18);
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // add a marker in the given location, attach some popup content to it and open the popup
-L.marker([50.60496, 3.14942]).addTo(map)
-  .bindPopup("<strong>INRIA Lille France</strong><br>Office: B320<br>Parc scientifique de la Haute Borne<br>40, avenue Halley - Building B - Park Plaza<br>59650 Villeneuve d'Ascq - France<br>")
+L.marker([50.63309, 3.02029]).addTo(map)
+  .bindPopup("<strong>Euratechnologies</strong><br>165, Avenue de Bretagne<br>59000 Lille - France<br>")
   .openPopup();
-  
